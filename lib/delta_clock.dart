d3aa3ee22bd27c57323b6dec43bf38121d509a85
Ahad Islam <ahad.islam624@gmail.com>
2020-01-08 15:44:46 -0500

    added an update timer to get the darn thing to work

--- lib/delta_clock.dart
+++ lib/delta_clock.dart
@@ -15,6 +15,16 @@ class DeltaClock extends StatefulWidget {
 
 class _DeltaClockState extends State<DeltaClock> {
   DateTime _dateTime = DateTime.now();
+  Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.clock.addListener(_updateTime);
+    _updateTime();
+  }
+
+  
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +35,20 @@ class _DeltaClockState extends State<DeltaClock> {
       color: Colors.blueGrey,
       child: Center(
         child: Text(DateFormat('HH:mm').format(_dateTime),
-            style: TextStyle(fontSize: fontSize)
-            ),
+            style: TextStyle(fontSize: fontSize)),
       ),
     );
   }
-}
 
+  void _updateTime() {
+    setState(() {
+      _dateTime = DateTime.now();
+      _timer = Timer(
+        Duration(minutes: 1) -
+            Duration(seconds: _dateTime.second) -
+            Duration(milliseconds: _dateTime.millisecond),
+        _updateTime,
+      );
+    });
+  }
+}
