45f93c2823a04708cedd9122b5900d17cd8f285e
Ahad Islam <ahad.islam624@gmail.com>
2020-01-08 13:55:48 -0500

    imported the flutter material, allows you to run the app added intl package to allow me to ISO format the date.

--- lib/delta_clock.dart
+++ lib/delta_clock.dart
@@ -1,25 +1,29 @@
-
-
 import 'dart:async';
-import 'package:flutter/material.dart';
+
 import 'customizer.dart';
 import 'model.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class DeltaClock extends StatefulWidget {
-  const DeltaClock(this._clock);
+  const DeltaClock(this.clock);
 
-  final ClockBuilder _clock;
+  final ClockModel clock;
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return null;
-  }
+  State<StatefulWidget> createState() => _DeltaClockState();
 }
 
 class _DeltaClockState extends State<DeltaClock> {
+  DateTime _dateTime = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    final fontSize = MediaQuery.of(context).size.width / 3.5;
+
+    return Center(
+      child: Text(DateFormat('HH:mm').format(_dateTime),
+          style: TextStyle(fontSize: fontSize)
+          ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 
 import 'package:flutter/material.dart';
+import 'delta_clock.dart';
 import 'customizer.dart';
 import 'model.dart';
 
 void main() {
+  runApp(ClockCustomizer((ClockModel model) => DeltaClock(model)));
 }
 
